42b80b3f9dd2dddc8a8e4e162e1be4a0fde0ed16
Eric Lagarda <Krato@users.noreply.github.com>
2021-01-20 13:41:08 +0100

    Fix routes namespace

--- src/routes.php
+++ src/routes.php
@@ -9,44 +9,43 @@ Route::group(
         'prefix' => LaravelLocalization::setLocale(),
         'middleware' => ['localize', 'localeSessionRedirect', 'localizationRedirect' ]
     ],
-    function()
-    {
-    	Route::get(LaravelLocalization::transRoute('routes.admin/blog').'/data_blog', 'starter\Blog\Controllers\PostController@data_blog');
-});
+    function () {
+        Route::get(LaravelLocalization::transRoute('routes.admin/blog').'/data_blog', 'App\Blog\Controllers\PostController@data_blog');
+    }
+);
 
 Route::group(
     [
         'prefix' => LaravelLocalization::setLocale(),
         'middleware' => ['localize', 'localeSessionRedirect', 'localizationRedirect' ]
     ],
-    function()
-    {
+    function () {
         //HOTELS
 
 
-    	Route::get(LaravelLocalization::transRoute('routes.admin/blog'), 'starter\Blog\Controllers\PostController@index');
-        Route::get(LaravelLocalization::transRoute('routes.admin/blog/trash'), 'starter\Blog\Controllers\PostController@index(true)');
-		Route::get(LaravelLocalization::transRoute('routes.admin/blog/new'), 'starter\Blog\Controllers\PostController@create');
-		Route::post(LaravelLocalization::transRoute('routes.admin/blog/store'), 'starter\Blog\Controllers\PostController@store');
-        Route::get(LaravelLocalization::transRoute('routes.admin/blog/edit'), 'starter\Blog\Controllers\PostController@edit');
-        Route::post(LaravelLocalization::transRoute('routes.admin/blog/update'), 'starter\Blog\Controllers\PostController@update');
-        Route::post(LaravelLocalization::transRoute('routes.admin/blog/remove'), 'starter\Blog\Controllers\PostController@destroy');
-        Route::post(LaravelLocalization::transRoute('routes.admin/blog/remove_force'), 'starter\Blog\Controllers\PostController@forceDestroy');
-        Route::post(LaravelLocalization::transRoute('routes.admin/blog/restore'), 'starter\Blog\Controllers\PostController@restore');
-		
-		Route::post(LaravelLocalization::transRoute('routes.admin/blog/categories/store'), 'starter\Blog\Controllers\CategoriesController@store');
-        Route::get(LaravelLocalization::transRoute('routes.admin/blog/categories/edit'), 'starter\Blog\Controllers\CategoriesController@edit');
-        Route::post(LaravelLocalization::transRoute('routes.admin/blog/categories/update'), 'starter\Blog\Controllers\CategoriesController@update');
-        Route::post(LaravelLocalization::transRoute('routes.admin/blog/categories/remove'), 'starter\Blog\Controllers\CategoriesController@destroy');
+        Route::get(LaravelLocalization::transRoute('routes.admin/blog'), 'App\Blog\Controllers\PostController@index');
+        Route::get(LaravelLocalization::transRoute('routes.admin/blog/trash'), 'App\Blog\Controllers\PostController@index(true)');
+        Route::get(LaravelLocalization::transRoute('routes.admin/blog/new'), 'App\Blog\Controllers\PostController@create');
+        Route::post(LaravelLocalization::transRoute('routes.admin/blog/store'), 'App\Blog\Controllers\PostController@store');
+        Route::get(LaravelLocalization::transRoute('routes.admin/blog/edit'), 'App\Blog\Controllers\PostController@edit');
+        Route::post(LaravelLocalization::transRoute('routes.admin/blog/update'), 'App\Blog\Controllers\PostController@update');
+        Route::post(LaravelLocalization::transRoute('routes.admin/blog/remove'), 'App\Blog\Controllers\PostController@destroy');
+        Route::post(LaravelLocalization::transRoute('routes.admin/blog/remove_force'), 'App\Blog\Controllers\PostController@forceDestroy');
+        Route::post(LaravelLocalization::transRoute('routes.admin/blog/restore'), 'App\Blog\Controllers\PostController@restore');
+        
+        Route::post(LaravelLocalization::transRoute('routes.admin/blog/categories/store'), 'App\Blog\Controllers\CategoriesController@store');
+        Route::get(LaravelLocalization::transRoute('routes.admin/blog/categories/edit'), 'App\Blog\Controllers\CategoriesController@edit');
+        Route::post(LaravelLocalization::transRoute('routes.admin/blog/categories/update'), 'App\Blog\Controllers\CategoriesController@update');
+        Route::post(LaravelLocalization::transRoute('routes.admin/blog/categories/remove'), 'App\Blog\Controllers\CategoriesController@destroy');
 
         //IMAGE TEXT EDITOR
-        Route::post('admin/blog/upload_editor_image', 'starter\Blog\Controllers\PostController@upload_image_editor');
-        Route::post('admin/blog/remove_editor_image', 'starter\Blog\Controllers\PostController@remove_editor_images');
+        Route::post('admin/blog/upload_editor_image', 'App\Blog\Controllers\PostController@upload_image_editor');
+        Route::post('admin/blog/remove_editor_image', 'App\Blog\Controllers\PostController@remove_editor_images');
         
-        Route::get(LaravelLocalization::transRoute('routes.menu.public_blog'), 'starter\Blog\Controllers\PostController@index_front');
+        Route::get(LaravelLocalization::transRoute('routes.menu.public_blog'), 'App\Blog\Controllers\PostController@index_front');
 
-        Route::get(LaravelLocalization::transRoute('routes.menu.public_blog_item'), 'starter\Blog\Controllers\PostController@show')->where('slug', '[a-z0-9-]+');
-        Route::get(LaravelLocalization::transRoute('routes.menu.public_blog_category'), 'starter\Blog\Controllers\PostController@showCategory')->where('slug', '[a-z0-9-]+');
-        Route::get(LaravelLocalization::transRoute('routes.menu.public_blog_tag'), 'starter\Blog\Controllers\PostController@showTag')->where('slug', '[a-z0-9-]+');
-
-});
+        Route::get(LaravelLocalization::transRoute('routes.menu.public_blog_item'), 'App\Blog\Controllers\PostController@show')->where('slug', '[a-z0-9-]+');
+        Route::get(LaravelLocalization::transRoute('routes.menu.public_blog_category'), 'App\Blog\Controllers\PostController@showCategory')->where('slug', '[a-z0-9-]+');
+        Route::get(LaravelLocalization::transRoute('routes.menu.public_blog_tag'), 'App\Blog\Controllers\PostController@showTag')->where('slug', '[a-z0-9-]+');
+    }
+);
